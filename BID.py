4a797af83b002510c76b9a2a00bedb1f43439a45
root <>
2017-09-24 13:40:34 +0000

    Added scan option

--- BID.py
+++ BID.py
@@ -5,6 +5,7 @@ version = "1.0"
 
 def usage():
     print("\n:::::::::::::::::::::::::BID usage guide::::::::::::::::::::::::")
+    print("[*] To find BSSID ( MAC ), perform scan;\n\t -python3 BID.py -s/--scan -i wlan0") 
     print("\n\t -python BID.py --mac,-m E8:40:F2:69:E6:3B  -c,--channel 0-14 -i,--interface wlan0 ")
     print("\n\t        If you don't get it fuck you")
 
@@ -24,6 +25,7 @@ def put_to_monitorMode(iface, channel):
         for line in io.TextIOWrapper(s1.stdout, encoding="utf-8"):
             print(line)
     return
+
 
 def turn_off_monitorMode(iface):
     print("[*]Closing script...\n")
@@ -50,7 +52,7 @@ def perform_denial(mac,iface,channel):
 
     put_to_monitorMode(iface, channel)
 
-    print("Performing attack!!!\n To stop press CTR+D")
+    print("Performing attack!!!\n To stop press CTR+C")
 
     
 
@@ -62,7 +64,38 @@ def perform_denial(mac,iface,channel):
         print(line)
 
     return
-    
+
+
+def put_to_monitorModeScan(iface):
+    print("Putting %s to monitor mode..."%iface)
+    #sleep(2)
+    cmd =["ifconfig "+str(iface)+" down",
+          "iwconfig "+iface+" mode monitor",
+          "ifconfig "+str(iface)+" up",
+          "airmon-ng check kill"]
+
+
+    for command in cmd:
+        s1 = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, shell=True)
+        for line in io.TextIOWrapper(s1.stdout, encoding="utf-8"):
+            print(line)
+    return
+
+def scan_APs(iface):
+    #put_to_monitorModeScan(iface)
+   
+    cmd = ["airodump-ng "+iface]
+
+    try:
+        scan = subprocess.Popen(cmd, shell=True,stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in io.TextIOWrapper(scan.stdout, encoding="utf-8"):
+            print(line)
+
+    except KeyboardInterrupt:
+        sys.exit(2)
+            
+        
+        
     
     
 def main():
@@ -72,7 +105,7 @@ def main():
     interface = ""
     
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hm:c:i:",["help","version","mac=","channel=","interface="])
+        opts, args = getopt.getopt(sys.argv[1:], "hsm:c:i:",["help","scan","version","mac=","channel=","interface="])
         #print(opts)
     except getopt.GetoptError as err:
         print(err)
@@ -107,19 +140,22 @@ def main():
             #print(channel)
         elif o in ("-v","--version"):
             print(version)
+        elif o in ("-s","--scan"):
+            scan_APs(interface)
         else:
             assert False," Option is not recognized!"
 
     print("Mac Address of AP: %s\nOn channel: %i\nInterface: %s"%(mac_address,int(channel),interface))
-
-    try:
-        perform_denial(mac_address, interface, channel)
+    if mac_address and interface is not "" and channel is not 0:
         
-    except KeyboardInterrupt:
-        turn_off_monitorMode(interface)
+        try:
+            perform_denial(mac_address, interface, channel)
+            
+        except KeyboardInterrupt:
+            turn_off_monitorMode(interface)
 
-        print("\nGoodbye...")
-        sys.exit(2)
+            print("\nGoodbye...")
+            sys.exit(2)
         
 
 if __name__=="__main__":
